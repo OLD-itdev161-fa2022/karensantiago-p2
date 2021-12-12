5dcb684b4b646a84f04a218d325bf15fa7155b09
santiak3 <71164211+santiak3@users.noreply.github.com>
2021-12-12 20:02:18 +0000

    Created API

---
+++ server/controller/controller.js
@@ -0,0 +1,21 @@
+var Userdb = require('../model/model');
+
+// Create and save new user
+exports.create = (req,res) => {
+
+}
+
+// Retrieve and return all users/ retrieve and return a single user
+exports.find = (req, res) => {
+
+}
+
+// Update a new identified user by user id
+exports.update = (req, res) => {
+
+}
+
+// Delete a user with specified user id in the request
+exports.delete = (req, res) => {
+    
+}
