22f831db9e1dd7c46c96e1c38accd59ecda7000c
santiak3 <71164211+santiak3@users.noreply.github.com>
2021-12-12 21:03:16 +0000

    Connected MongoDB data to application

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -33,22 +33,22 @@ exports.create = (req,res) => {
 
 // Retrieve and return all users/ retrieve and return a single user
 exports.find = (req, res) => {
-    if(req.query.id){
+    if(req.query.id) {
         const id = req.query.id;
 
         Userdb.findById(id)
-            .then(data =>{
-                if(!data){
-                    res.status(404).send({ message : "Not found user with id "+ id})
-                }else{
+            .then(data => {
+                if(!data) {
+                    res.status(404).send({ message : "Not found user with id "+ id })
+                } else {
                     res.send(data)
                 }
             })
-            .catch(err =>{
-                res.status(500).send({ message: "Erro retrieving user with id " + id})
+            .catch(err => {
+                res.status(500).send({ message: "Error retrieving user with id " + id })
             })
 
-    }else{
+    } else {
         Userdb.find()
             .then(user => {
                 res.send(user)
