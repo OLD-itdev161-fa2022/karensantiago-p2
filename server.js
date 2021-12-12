c27f34bfd26b187b04f1c776d6dd17ac3cf9a790
santiak3 <71164211+santiak3@users.noreply.github.com>
2021-12-12 12:10:02 +0000

    Added body parts and modules

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 // Allows us to separate our secret from our source code
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require('path');
+
 
 const app = express();
 
@@ -11,7 +14,24 @@ dotenv.config({ path: 'config.env'})
 const PORT = process.env.PORT || 8080
 
 // Log Requests
-app.use(morgan('tiny'));
+app.use(
+    morgan(
+        'tiny'));
+
+// Parse request to body-parser
+app.use(
+    bodyparser.urlencoded({
+        extended:true
+    }))
+
+// Set view engine
+app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname,"views/ejs"))
+
+// Load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
 app.get('/', (req, res) => {
     res.send("Project 2 Application");
